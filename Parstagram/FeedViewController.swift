5fc20631a60ab890c4c6442599ef59e3ad55b06b
Valli Mahavadi <smahava@ncsu.edu>
2021-03-27 13:09:30 -0400

    displaying comments

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -33,7 +33,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         super.viewDidAppear(animated)
         
         let query = PFQuery(className: "Posts");
-        query.includeKey("author");
+        query.includeKeys(["author", "comments", "comments.author"]);
         numOfPosts = 20;
         query.limit = numOfPosts;
         
@@ -47,12 +47,26 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        <#code#>
+        let post = posts[indexPath.section]
+        let comment = PFObject(className: "Comments")
+        comment["text"] = "This is the first post"
+        comment["post"] = post
+        comment["author"] = PFUser.current()!
+        
+        post.add(comment, forKey: "comments")
+        
+        post.saveInBackground { (success, error) in
+            if success {
+                print("Comment saved!")
+            } else {
+                print("Error saving post.")
+            }
+        }
     }
     
     func loadMorePosts() {
         let query = PFQuery(className: "Posts");
-        query.includeKey("author");
+        query.includeKeys(["author", "comments", "comments.author"]);
         numOfPosts = numOfPosts + 5;
         query.limit = numOfPosts;
         
@@ -65,25 +79,46 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return posts.count;
+        let post = posts[section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
+        return comments.count + 1;
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PostTableViewCell") as! PostTableViewCell;
-        let rowNum = posts.count - indexPath.row - 1;
-        let post = posts[rowNum];
-        let user = post["author"] as! PFUser;
-        cell.usernameLabel.text = user.username;
-        cell.headerUserLabel.text = user.username;
-        cell.captionLabel.text = post["caption"] as? String;
+        //let rowNum = posts.count - indexPath.section - 1;
+        //let post = posts[rowNum];
+        let post = posts[indexPath.section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
         
-        let imageFile = post["image"] as! PFFileObject;
-        let urlString = imageFile.url!
-        let url = URL(string: urlString)!
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PostTableViewCell") as! PostTableViewCell;
+            let user = post["author"] as! PFUser;
+            cell.usernameLabel.text = user.username;
+            cell.headerUserLabel.text = user.username;
+            cell.captionLabel.text = post["caption"] as? String;
+            
+            let imageFile = post["image"] as! PFFileObject;
+            let urlString = imageFile.url!
+            let url = URL(string: urlString)!
+            
+            cell.photoView.af.setImage(withURL: url);
+            
+            return cell;
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CommentCell") as! CommentCell;
+            let comment = comments[indexPath.row - 1]
+            cell.commentLabel.text = comment["text"] as! String
+            
+            let user = comment["author"] as! PFUser
+            cell.nameLabel.text = user.username
+            
+            return cell
+        }
         
-        cell.photoView.af.setImage(withURL: url);
-        
-        return cell;
     }
     
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
